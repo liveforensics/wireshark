29c45f2f6b55a5ce539d2e39bc3d75e342b319f7
liveforensics <liveforensicsuk@gmail.com>
2020-11-15 10:42:22 +0000

    update

--- build.ps1
+++ build.ps1
@@ -1,53 +1,60 @@
-Write-Host "Build script based on instructions @ https://www.wireshark.org/docs/wsdg_html_chunked/ChSetupWin32.html"
+function Write-Message {
+    Param ([String]$message)
 
-Write-Host "Installing chocolatey"
+    if ($message)
+    {
+        Write-Host "$([char]0x1b)[33m*********************************************************************************************************************************************" -ForegroundColor Yellow
+        Write-Host "*"
+        Write-Host "* " $message
+        Write-Host "*"
+        Write-Host "*********************************************************************************************************************************************"
+        Write-Host "$([char]0x1b)[37m "
+    }
+}
+
+Write-Message "Build script based on instructions @ https://www.wireshark.org/docs/wsdg_html_chunked/ChSetupWin32.html"
+
+Write-Message "Installing chocolatey"
 Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
-Write-Host "Installing Python 3"
+Write-Message "Installing Python 3"
 choco install -y python3
 
-Write-Host "Installing Nano - because, well why not!"
+Write-Message "Installing Nano - because, well why not!"
 choco install -y nano
 
-Write-Host "Installing Perl - eek!"
+Write-Message "Installing Perl - eek!"
 choco install -y activeperl
 # choco install -y strawberryperl
 
-Write-Host "Installing git"
+Write-Message "Installing git"
 choco.exe install -y git
 
-Write-Host "Installing Java"
+Write-Message "Installing Java"
 choco install -y openjdk11
 
-Write-Host "Installing CMake"
+Write-Message "Installing CMake"
 choco install -y cmake
 
-Write-Host "Installing Asciidoctor"
+Write-Message "Installing Asciidoctor"
 choco install -y asciidoctorj xsltproc docbook-bundle
 
-Write-Host "Installing winflexbison"
+Write-Message "Installing winflexbison"
 choco install -y winflexbison3
 
-Write-Host "Creating development folder"
+Write-Message "Creating development folders"
 New-Item -ItemType Directory -Path c:\Development
 New-Item -ItemType Directory -Path c:\Development\wsbuild64
 
-Write-Host "Installing vs community edition"
+Write-Message "Installing vs community edition"
 choco install -y visualstudio2019community visualstudio2019-workload-nativedesktop
 
-# if(Test-Path 'c:/temp/Qt5.12.9.zip')
-# {
-#     Write-Host "Installing QT"
-#     Expand-Archive -Path 'c:/temp/Qt5.12.9.zip' -DestinationPath 'c:\qt';
-#     Remove-Item -Path 'c:/temp/Qt5.12.9.zip' -Force
-# }
-
-Write-Host "Pulling NSIS"
+Write-Message "Pulling NSIS"
 $downloadUrl = "https://nav.dl.sourceforge.net/project/nsis/NSIS%203/3.06.1/nsis-3.06.1-setup.exe"
 Invoke-WebRequest -Uri $downloadUrl -OutFile c:\temp\nsis.exe
 if(Test-Path c:\temp\nsis.exe)
 {
-    Write-Host "Installing NSIS"
+    Write-Message "Installing NSIS"
     Set-Location C:\Temp
     .\nsis.exe /S /NCRC /D=c:\nsis
 }
